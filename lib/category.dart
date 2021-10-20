11210650cc59e0fad7a5558ba346df1566c5bcfd
user <ram8948@gmail.com>
2021-10-20 13:13:39 +0530

    category page load more work

--- lib/category.dart
+++ lib/category.dart
@@ -1,7 +1,5 @@
 import 'dart:convert';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:cached_network_image/cached_network_image.dart';
+import 'dart:developer';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -10,84 +8,145 @@ class Category extends StatefulWidget {
   const Category({Key? key}) : super(key: key);
 
   @override
-  _CategoryState createState() => _CategoryState();
+  CategoryState createState() => CategoryState();
 }
 
-class _CategoryState extends State<Category> {
-  late List data;
+class CategoryState extends State<Category> {
+  // We will fetch data from this Rest api
+  final _baseUrl = 'https://jsonplaceholder.typicode.com/posts';
 
-  // Function to get the JSON data
-  Future<String> getJSONData() async {
-    var response = await http.get(
-      // Encode the url
-        Uri.parse("https://unsplash.com/napi/photos/Q14J2k8VE3U/related"),
-        // Only accept JSON response
-        headers: {"Accept": "application/json"}
-    );
+  // At the beginning, we fetch the first 20 posts
+  int _page = 0;
+  final int _limit = 20;
+
+  // There is next page or not
+  bool _hasNextPage = true;
+
+  // Used to display loading indicators when _firstLoad function is running
+  bool _isFirstLoadRunning = false;
+
+  // Used to display loading indicators when _loadMore function is running
+  bool _isLoadMoreRunning = false;
+
+  // This holds the posts fetched from the server
+  List _posts = [];
+
+  // This function will be called when the app launches (see the initState function)
+  void _firstLoad() async {
+    setState(() {
+      _isFirstLoadRunning = true;
+    });
+    try {
+      final res =
+      await http.get(Uri.parse("$_baseUrl?_page=$_page&_limit=$_limit"));
+      setState(() {
+        _posts = json.decode(res.body);
+      });
+    } catch (err) {
+      inspect('Something went wrong');
+    }
 
     setState(() {
-      // Get the JSON data
-      data = json.decode(response.body)['results'];
+      _isFirstLoadRunning = false;
     });
+  }
 
-    return "Successfull";
+  // This function will be triggered whenver the user scroll
+  // to near the bottom of the list view
+  void _loadMore() async {
+    if (_hasNextPage == true &&
+        _isFirstLoadRunning == false &&
+        _isLoadMoreRunning == false &&
+        _controller.position.extentAfter < 300) {
+      setState(() {
+        _isLoadMoreRunning = true; // Display a progress indicator at the bottom
+      });
+      _page += 1; // Increase _page by 1
+      try {
+        final res =
+        await http.get(Uri.parse("$_baseUrl?_page=$_page&_limit=$_limit"));
+
+        final List fetchedPosts = json.decode(res.body);
+        if (fetchedPosts.isNotEmpty) {
+          setState(() {
+            _posts.addAll(fetchedPosts);
+          });
+        } else {
+          // This means there is no more data
+          // and therefore, we will not send another GET request
+          setState(() {
+            _hasNextPage = false;
+          });
+        }
+      } catch (err) {
+        inspect('Something went wrong!');
+      }
+
+      setState(() {
+        _isLoadMoreRunning = false;
+      });
+    }
+  }
+
+  // The controller for the ListView
+  late ScrollController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _firstLoad();
+    _controller = ScrollController()..addListener(_loadMore);
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(_loadMore);
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: _buildListView(),
-    );
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Name"),
+      body: _isFirstLoadRunning
+          ? const Center(
+        child: CircularProgressIndicator(),
+      )
+          : Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              controller: _controller,
+              itemCount: _posts.length,
+              itemBuilder: (_, index) => Card(
+                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+                child: ListTile(
+                  title: Text(_posts[index]['title']),
+                  subtitle: Text(_posts[index]['body']),
+                ),
+              ),
+            ),
+          ),
+
+          // when the _loadMore function is running
+          if (_isLoadMoreRunning == true)
+            const Padding(
+              padding: EdgeInsets.only(top: 10, bottom: 40),
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+
+          // When nothing else to load
+          if (_hasNextPage == false)
+            Container(
+              padding: const EdgeInsets.only(top: 30, bottom: 40),
+              color: Colors.amber,
+              child: const Center(
+                child: Text('You have fetched all of the content'),
+              ),
+            ),
+        ],
       ),
-      body: _buildListView(),
     );
-  }
-
-  Widget _buildListView() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemCount: data == null ? 0 : data.length,
-        itemBuilder: (context, index) {
-          return _buildImageColumn(data[index]);
-          // return _buildRow(data[index]);
-        }
-    );
-  }
-
-  Widget _buildImageColumn(dynamic item) => Container(
-    decoration: BoxDecoration(
-        color: Colors.white54
-    ),
-    margin: const EdgeInsets.all(4),
-    child: Column(
-      children: [
-        new CachedNetworkImage(
-          imageUrl: item['urls']['small'],
-          placeholder: (context, url) => new CircularProgressIndicator(),
-          errorWidget: (context, url, error) => new Icon(Icons.error),
-          fadeOutDuration: new Duration(seconds: 1),
-          fadeInDuration: new Duration(seconds: 3),
-        ),
-        _buildRow(item)
-      ],
-    ),
-  );
-
-  Widget _buildRow(dynamic item) {
-    return ListTile(
-      title: Text(
-        item['description'] == null ? '': item['description'],
-      ),
-      subtitle: Text("Likes: " + item['likes'].toString()),
-    );
-  }
-  @override
-  void initState() {
-    super.initState();
-    // Call the getJSONData() method when the app initializes
-    this.getJSONData();
   }
 }
